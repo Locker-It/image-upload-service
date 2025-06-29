db88c823c4ec87033702902fcd8c3100fc58d54a
OfekSagiv <ofeksagiv@gmail.com>
2025-06-29 22:06:26 +0300

    added status code package

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,19 +1,20 @@
-const { generatePresignedUploadUrl } = require('../services/s3.service');
-const { ERROR_MESSAGES } = require("../constants/errorMessages");
+const {generatePresignedUploadUrl} = require('../services/s3.service');
+const {ERROR_MESSAGES} = require("../constants/errorMessages");
+const {StatusCodes} = require("http-status-codes");
 
 async function getUploadUrl(req, res) {
     try {
-        const { filename, mimetype } = req.body;
+        const {filename, mimetype} = req.body;
 
         if (!filename || !mimetype) {
-            return res.status(400).json({ error: ERROR_MESSAGES.MISSING_METADATA });
+            return res.status(StatusCodes.BAD_REQUEST).json({error: ERROR_MESSAGES.MISSING_METADATA});
         }
 
-        const { uploadUrl, publicUrl } = await generatePresignedUploadUrl(filename, mimetype);
-        res.json({ uploadUrl, publicUrl });
+        const {uploadUrl, publicUrl} = await generatePresignedUploadUrl(filename, mimetype);
+        res.json({uploadUrl, publicUrl});
     } catch (error) {
-        res.status(500).json({ error: error.message || ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error: error.message || ERROR_MESSAGES.INTERNAL_SERVER_ERROR});
     }
 }
 
-module.exports = { getUploadUrl };
+module.exports = {getUploadUrl};
